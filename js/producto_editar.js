1303661713144e4b1713e31724e208957f542730
Ivan Bravo <116473927+ivann-bravo@users.noreply.github.com>
2023-07-07 02:01:18 -0300

    Update producto_editar.js

--- js/producto_editar.js
+++ js/producto_editar.js
@@ -10,7 +10,7 @@ createApp({
             imagen: "",
             stock: 0,
             precio: 0,
-            url: 'http://ibravo.pythonanywhere.com/productos/' + id,
+            url: 'https://ibravo.pythonanywhere.com/productos/' + id,
         }
     },
     methods: {
